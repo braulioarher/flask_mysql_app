768a5b134f451e5eae8d25d6bfee3fad2c8a8810
Braulio Arcos <braulio.arher@gmail.com>
2022-10-11 16:24:05 -0500

    Se agrega una pestana a la barra de navegacion para usar quick forms de wtf

--- roomapp/routes.py
+++ roomapp/routes.py
@@ -1,10 +1,19 @@
 from crypt import methods
 from datetime import datetime
+from multiprocessing import context
 from roomapp import app, db
 from roomapp import render_template, request
 from roomapp.models import Temperature
 from .forms import TempForm
 from datetime import datetime
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error = error)
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error = error)
 
 @app.route('/')
 def index():
@@ -36,3 +45,22 @@ def addtemp():
             db.session.rollback()
         
     return render_template('addtemp.html', form=form)
+
+@app.route("/addtempwtf/", methods=['GET', 'POST'])
+def addtempwtf():
+    temp_form = TempForm()
+    if request.method == 'POST':
+        datenow = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        temp =  Temperature(Date=datenow, DeviceID=int(temp_form.DeviceID.data), Temperature=float(temp_form.Temperature.data))
+        try:
+            db.session.add(temp)
+            db.session.commit()
+            return render_template('addtemp_confirmation.html')
+        except Exception:
+            db.session.rollback()
+    context = {
+        'title' : 'Add New Temperature',
+        'temp_form' : temp_form
+    }
+        
+    return render_template('addtempwtfquick.html', **context)
